c593ddbd20436d3c5a6bfb415764bdabe92ae344
Andrei <andreirech22@outlook.com>
2024-10-23 15:14:29 -0300

    alterando para aceitar arquivos

--- exec.bat
+++ exec.bat
@@ -1,12 +1,19 @@
 @echo off
 
-rem Executa o CUP para gerar o analisador sintático
+rem
 java -cp java-cup-11b.jar java_cup.Main -parser parser -symbols Sym analisadorsintatico.cup
 
-rem Compila os arquivos Java gerados (Lexer.java, parser.java e Sym.java)
+rem
 javac -cp "java-cup-11b-runtime.jar;." Lexer.java parser.java Sym.java
 
-rem Executa o analisador sintático
-java -cp "java-cup-11b-runtime.jar;." parser
+rem
+if "%1"=="" (
+    echo Por favor, forneça o arquivo de entrada .txt.
+    pause
+    exit /b
+)
+
+rem
+java -cp "java-cup-11b-runtime.jar;." parser %1
 
 pause
